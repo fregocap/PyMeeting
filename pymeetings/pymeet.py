ce00afab14bb184ae50e0f61449cd52814b50c17
Fabio Capela <capela625@gmail.com>
2020-11-15 22:09:58 +0100

    adding the basis of the main class

---
+++ pymeetings/pymeet.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+
+class GetMeetingHour():
+    
+    def __init__(self,vec_cities,day):
+        """
+        Computes the most interesting hours for a particular day, considering 
+        that you provide several cities
+        
+        :param vec 
+        """
+        
+        
