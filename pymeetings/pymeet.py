edb672dad58b21a2109a7d35cb16fae800b9b5f6
Fabio Capela <capela625@gmail.com>
2020-11-15 23:38:51 +0100

    adding a function to get the hours computed

--- pymeetings/pymeet.py
+++ pymeetings/pymeet.py
@@ -3,6 +3,7 @@ import numpy as np
 from pymeetings.utils import *
 import pytz
 from geopy import geocoders
+from datetime import datetime
 
 
 class GetMeetingHour():
@@ -13,16 +14,12 @@ class GetMeetingHour():
         that you provide several cities
         
         :param vec cities: the number of cities to consider for the algorithm 
-        :param date day  : the date to make the calculation 
+        :param str day  : the date to make the calculation in the format: 2020/02/20 
         """
 
         self.cities_vec = cities
         self.day = day
         self.g = geocoders.GoogleV3()
-
-        
-    def BestHour(self):
-        pass
 
 
     def Countries(self):
@@ -56,6 +53,42 @@ class GetMeetingHour():
                 timezones.append(None)
 
         return timezones
+
+
+    def checkdate(self,date):
+        """
+        Function that checks that the date string is in the right format
+        """
         
+    
+    def Hours(self):
+        """
+        Function that calculates the vector of times for a particular day
+        """
         
+        hours = []
+
+        if checkdate(self.day):
+            for j in range(0, 24):
+                if j<10:
+                    hours.append(self.day+' '+'0'+str(j)+':00')
+                else:
+                    hours.append(self.day+' '+str(j)+':00')
+
+        return hours
+
+                    
+    def BestHour(self):
+        """
+        Function that computes the best hour for the meeting, the `feel lucky` guess
+        """
+        pass
+    
+
+    def ConstructTable(self):
+        """
+        Function that provides the table of interest 
+        """
+        df = pd.DataFrame({'Hour':self.Hours()})
         
+
