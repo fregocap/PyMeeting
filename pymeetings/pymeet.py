029ec696bb4544f630825655ed55fffb4f0e1b72
Fabio Capela <capela625@gmail.com>
2020-11-15 22:21:26 +0100

    change small detail

--- pymeetings/pymeet.py
+++ pymeetings/pymeet.py
@@ -4,12 +4,12 @@ import numpy as np
 
 class GetMeetingHour():
     
-    def __init__(self,vec_cities,day):
+    def __init__(self,cities,day):
         """
         Computes the most interesting hours for a particular day, considering 
         that you provide several cities
         
-        :param vec 
+        :param vec cities: 
         """
         
         
